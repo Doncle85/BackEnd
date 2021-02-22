1dccce08302ff408b2159601847338c9886e4331
az02754 <clement.c.malarme@renault.com>
2021-02-22 11:14:40 +0100

    Merge branch 'master' of C:\Users\az02754\Documents\ProjetB with conflicts.

--- src/main/java/com/example/demo/controller/BetController.java
+++ src/main/java/com/example/demo/controller/BetController.java
@@ -1,11 +1,15 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.BetDto;
+import com.example.demo.entity.Bet;
+import com.example.demo.repository.BetRepository;
 import com.example.demo.service.BetService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BetController {
@@ -17,7 +21,14 @@ public class BetController {
         betService.save(bet);
     }
 
+    @GetMapping("/bets")
+    List<Bet> all() {
+        return betService.findAll();
+    }
 
-
-
+    @DeleteMapping("/bets/{id}")
+    void deleteBets(@PathVariable ("id") int id) {
+        betService.deleteById(id);
+    }
 }
+
